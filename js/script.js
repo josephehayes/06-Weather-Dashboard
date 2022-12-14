0681382866c2266fd9af1e8c28f5536c61071d40
Joseph Hayes <joey.hayes.1996@gmail.com>
2022-12-13 19:37:08 -0700

    1st big push

--- js/script.js
+++ js/script.js
@@ -1,12 +1,27 @@
 const weatherApi = "https://api.openweathermap.org/data/2.5/forecast?"
+const geoApi = "http://api.openweathermap.org/geo/1.0/direct?"
 const apiKey = "&appid=823a370fc2fa4cfdd918f24dc80857d7"
 
-function getWeather(lat, lon) {
-    fetch(weatherApi + "lat=" + lat + "&lon=" + lon + apiKey)
-    .then(response => response.json)
-    .then(result => {
-        console.log(result)
-    })
+let searchHistory = JSON.parse(localStorage.getItem("searchHist")) ?? [];
+
+function getWeather(city) {
+    if (!searchHistory.includes(city)) {
+        searchHistory.push(city);
+        console.log("Search History Added: " + city)
+        localStorage.setItem("searchHist", JSON.stringify(searchHistory))
+    }
+    fetch(weatherApi + "units=imperial&q=" + city + apiKey)
+        .then(response => response.json())
+        .then(result => {
+            console.table(result)
+        })
 }
 
-// getWeather(72, 4);
+function main() {
+    localStorage.clear()
+    getWeather("Jacksonville");
+    getWeather("Los Angeles");
+    console.log("history: ", searchHistory)
+}
+
+main();
