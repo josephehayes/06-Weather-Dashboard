457d021cd4ea9b14fe54530d271604041d407dca
Joseph Hayes <joey.hayes.1996@gmail.com>
2022-12-15 18:22:31 -0700

    Finished some styling and JS functions to display current date weather

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ let searchVal;
 const fiveDayCards = document.getElementsByClassName("forecast-card");
 const searchInput = document.getElementById("city-search");
 const historyCard = document.getElementById("search-history");
+const searchOutput = document.getElementById("search-output-container");
 
 function getWeather(city) {
     fetch(weatherApi + "units=imperial&q=" + city + apiKey)
@@ -17,13 +18,25 @@ function getWeather(city) {
             console.table(result)
             if (result.cod != 404) { //if city exists
                 
+                city = city.toUpperCase();
                 if (!searchHistory.includes(city)) {
                     searchHistory.push(city);
                     localStorage.setItem("searchHist", JSON.stringify(searchHistory));
                     historyCard.innerHTML = "";
                     displayHistory();
                 }
-                
+
+                //Add current day in big section
+
+                let todayOutEls = document.getElementsByClassName("search-today-output");
+                console.log(todayOutEls)
+
+                todayOutEls[0].innerHTML = "<p> Today, " + (result.list[3].dt_txt).substring(0, 10) + "</p>";
+                todayOutEls[1].innerHTML = "<img class='weather-icon' src='http://openweathermap.org/img/wn/" + result.list[3].weather[0].icon + "@2x.png'></img>";
+                todayOutEls[2].innerHTML = "<p> Temp: " + result.list[3].main.temp + " F </p>"
+                todayOutEls[3].innerHTML = "<p> Humidity: " + result.list[3].main.humidity + "% </p>"
+                todayOutEls[4].innerHTML = "<p> Wind: " + result.list[3].wind.speed + "% </p>"
+
                 curCard = 0;
 
                 //every 8th result is noon of each day
@@ -49,11 +62,12 @@ function displayHistory() {
     //     output += "<p class='history-item' onclick=getWeather('" + city + "')>" + city + "</p>";
     // })
     // historyCard.innerHTML = output;
+    historyCard.innerHTML = "";
     searchHistory.forEach(city => {
         let historyItem = document.createElement("p");
         historyItem.textContent = city;
         historyItem.id = city;
-        historyItem.className = 'history-item';
+        historyItem.className = 'history-item py-1 my-2';
         historyCard.append(historyItem)
     })
 }
